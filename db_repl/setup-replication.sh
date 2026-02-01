e4d70c752180ead9b3a9edcc6247ea87818ea32a
DrDre888 <azizakramzonov5@gmail.com>
2026-02-01 18:29:11 +0000

    edit

--- db_repl/setup-replication.sh
+++ db_repl/setup-replication.sh
@@ -4,4 +4,4 @@ until PGPASSWORD=$PGPASSWORD psql -h "$MASTER_HOST" -U "$PGUSER" -c '\q' 2>/dev/
   sleep 2
 done
 rm -rf /var/lib/postgresql/data/*
-PGPASSWORD=replicator_password pg_basebackup -h $MASTER_HOST -D /var/lib/postgresql/data -U replicator -P -v -R -X stream -C -S replica_slot
+PGPASSWORD=123 pg_basebackup -h $MASTER_HOST -D /var/lib/postgresql/data -U replicator -P -v -R -X stream
