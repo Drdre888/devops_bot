667fa43cd9602ed36cfce128906b3bee65602017
Drdre888 <azizakramzonov5@gmail.com>
2026-02-02 15:23:19 +0300

    Delete db_repl directory

--- db_repl/setup-replication.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-until PGPASSWORD=$PGPASSWORD psql -h "$MASTER_HOST" -U "$PGUSER" -c '\q' 2>/dev/null; do
-  sleep 2
-done
-rm -rf /var/lib/postgresql/data/*
-PGPASSWORD=123 pg_basebackup -h $MASTER_HOST -D /var/lib/postgresql/data -U replicator -P -v -R -X stream
